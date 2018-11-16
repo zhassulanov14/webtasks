0a9f76706bc1bd9ae3018177b6c74dbc8abe3db1
zhassulanov14 <43115825+zhassulanov14@users.noreply.github.com>
2018-11-16 22:35:50 +0600

    Add files via upload

---
+++ week9/task-2.php
@@ -0,0 +1,37 @@
+<a href="task92.php?category=sport">Sport news</a> | <a href="task92.php?category=politics">Politic news</a><br/><br/>
+
+<a href="task92.php?category=sport&format=json">Sport news (JSON)</a> | <a href="task92.php?category=politics&format=json">Politic news (JSON)</a><br/><br/>
+
+<?php
+
+$news = ["sport"=>["C. Ronaldo has scored three goals in last five matches","Golovkin has won match for title"],"politics"=>["Trump has cancelled his visit to North Corea, because of sanction","N. Nazarbayev has approved new version of alphabet"]];
+
+$category = "sport";
+
+if (isset($_GET['category'])) {
+
+    $category = $_GET['category'];
+
+    if (isset($_GET['format'])) {
+
+        $format = $_GET['format'];
+
+        print json_encode($news[$category]);
+
+    }
+
+    else {
+
+        foreach($news[$category] as $new) {
+
+            print $new . "<hr>";
+
+        }
+
+    }
+
+}
+
+
+
+?>
